befc15069c08175ad164457d90a36d7532ac2f9f
nad <58147198+nad2040@users.noreply.github.com>
2023-02-21 23:08:44 -0500

    fixed mixin issue so mod works now

--- src/main/java/com/nad2040/elytrabombing/mixin/ItemMixin.java
+++ src/main/java/com/nad2040/elytrabombing/mixin/ItemMixin.java
@@ -1,6 +1,6 @@
 package com.nad2040.elytrabombing.mixin;
 
-import com.nad2040.elytrabombing.IUseItemInFlight;
+import com.nad2040.elytrabombing.ElytraBombingMod;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.FallingBlockEntity;
 import net.minecraft.entity.TntEntity;
@@ -19,53 +19,53 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Item.class)
-public class ItemMixin implements IUseItemInFlight {
-	@Override
-	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+public class ItemMixin {
+	@Inject(at = @At("HEAD"), method = "use", cancellable = true)
+	public void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
 		if (user.isFallFlying()) {
+			ElytraBombingMod.LOGGER.info("right click action detected");
 			ItemStack item = user.getStackInHand(hand);
 			Hand other_hand = hand == Hand.MAIN_HAND ? Hand.OFF_HAND : Hand.MAIN_HAND;
 			Vec3d position,velocity;
 			if (item.isOf(Items.FLINT_AND_STEEL)) {
 				ItemStack tnt = user.getStackInHand(other_hand);
-				if (!tnt.isOf(Items.TNT)) return TypedActionResult.pass(user.getStackInHand(hand));
-				if (!world.isClient) {
-					position = user.getPos();
-					velocity = user.getVelocity();
-					TntEntity tntEntity = new TntEntity(world, position.x, position.y, position.z, user);
-					tntEntity.setVelocity(velocity.multiply(1.2));
-					world.spawnEntity(tntEntity);
-					world.playSound(null, tntEntity.getX(), tntEntity.getY(), tntEntity.getZ(), SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0f, 1.0f);
-					world.emitGameEvent(user, GameEvent.PRIME_FUSE, position);
-					if (!user.getAbilities().creativeMode) {
-						item.damage(1, user, p -> p.sendToolBreakStatus(hand));
-						tnt.decrement(1);
-					}
-					user.incrementStat(Stats.USED.getOrCreateStat((FlintAndSteelItem)(Object)this));
+				if (!tnt.isOf(Items.TNT)) cir.setReturnValue(TypedActionResult.pass(user.getStackInHand(hand)));
+				position = user.getPos();
+				velocity = user.getVelocity();
+				TntEntity tntEntity = new TntEntity(world, position.x, position.y, position.z, user);
+				tntEntity.setVelocity(velocity.multiply(1.2));
+				world.spawnEntity(tntEntity);
+				world.playSound(null, tntEntity.getX(), tntEntity.getY(), tntEntity.getZ(), SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0f, 1.0f);
+				world.emitGameEvent(user, GameEvent.PRIME_FUSE, position);
+				if (!user.getAbilities().creativeMode) {
+					item.damage(1, user, p -> p.sendToolBreakStatus(hand));
+					tnt.decrement(1);
 				}
-				return TypedActionResult.success(user.getStackInHand(hand), world.isClient());
+				user.incrementStat(Stats.USED.getOrCreateStat((FlintAndSteelItem)(Object)this));
+				cir.setReturnValue(TypedActionResult.success(user.getStackInHand(hand), world.isClient()));
 			}
 			else if (item.isOf(Items.ANVIL) || item.isOf(Items.CHIPPED_ANVIL) || item.isOf(Items.DAMAGED_ANVIL)) {
-				if (!user.getStackInHand(other_hand).isEmpty()) return TypedActionResult.pass(user.getStackInHand(hand));
-				if (!world.isClient) {
-					position = user.getPos();
-					velocity = user.getVelocity();
-					FallingBlockEntity anvilEntity = null;
-					if (item.isOf(Items.ANVIL)) anvilEntity = FallingBlockEntity.spawnFromBlock(world, new BlockPos(position), Blocks.ANVIL.getDefaultState());
-					else if (item.isOf(Items.CHIPPED_ANVIL)) anvilEntity = FallingBlockEntity.spawnFromBlock(world, new BlockPos(position), Blocks.CHIPPED_ANVIL.getDefaultState());
-					else if (item.isOf(Items.DAMAGED_ANVIL)) anvilEntity = FallingBlockEntity.spawnFromBlock(world, new BlockPos(position), Blocks.DAMAGED_ANVIL.getDefaultState());
-					assert anvilEntity != null;
-					anvilEntity.setVelocity(velocity.multiply(1.2));
-					world.spawnEntity(anvilEntity);
-					world.playSound(null, anvilEntity.getX(), anvilEntity.getY(), anvilEntity.getZ(), SoundEvents.BLOCK_ANVIL_FALL, SoundCategory.BLOCKS, 1.0f, 1.0f);
-					if (!user.getAbilities().creativeMode) {
-						item.decrement(1);
-					}
+				if (!user.getStackInHand(other_hand).isEmpty()) cir.setReturnValue(TypedActionResult.pass(user.getStackInHand(hand)));
+				position = user.getPos();
+				velocity = user.getVelocity();
+				FallingBlockEntity anvilEntity = null;
+				if (item.isOf(Items.ANVIL)) anvilEntity = FallingBlockEntity.spawnFromBlock(world, new BlockPos(position), Blocks.ANVIL.getDefaultState());
+				else if (item.isOf(Items.CHIPPED_ANVIL)) anvilEntity = FallingBlockEntity.spawnFromBlock(world, new BlockPos(position), Blocks.CHIPPED_ANVIL.getDefaultState());
+				else if (item.isOf(Items.DAMAGED_ANVIL)) anvilEntity = FallingBlockEntity.spawnFromBlock(world, new BlockPos(position), Blocks.DAMAGED_ANVIL.getDefaultState());
+				assert anvilEntity != null;
+				anvilEntity.setVelocity(velocity.multiply(1.2));
+				world.spawnEntity(anvilEntity);
+				world.playSound(null, anvilEntity.getX(), anvilEntity.getY(), anvilEntity.getZ(), SoundEvents.BLOCK_ANVIL_FALL, SoundCategory.BLOCKS, 1.0f, 1.0f);
+				if (!user.getAbilities().creativeMode) {
+					item.decrement(1);
 				}
 			}
 		}
-		return TypedActionResult.pass(user.getStackInHand(hand));
+		cir.setReturnValue(TypedActionResult.pass(user.getStackInHand(hand)));
 	}
 }
