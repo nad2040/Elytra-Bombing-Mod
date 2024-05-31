cc5b2606320ace0370dd8a5bd410e1ea4c7b977e
nad <58147198+nad2040@users.noreply.github.com>
2024-05-30 20:09:46 -0400

    allow anvil dropping with items in other hand

--- src/main/java/com/nad2040/elytrabombing/ElytraBombingMod.java
+++ src/main/java/com/nad2040/elytrabombing/ElytraBombingMod.java
@@ -8,7 +8,6 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,10 +27,6 @@ public class ElytraBombingMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
         LOGGER.info("Elytra Bombing Mod initialized!");
-	}
-
-	public static BlockPos VEC_3D_TO_POS(Vec3d vec3d) {
-		return new BlockPos((int)Math.round(vec3d.x), (int)Math.round(vec3d.y), (int)Math.round(vec3d.z));
 	}
 
 	public static void log(Hand hand, Hand other_hand, ItemStack usedItemStack, ItemStack otherItemStack, Vec3d position, Vec3d velocity) {
