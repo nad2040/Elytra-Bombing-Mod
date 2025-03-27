f0225dc64ec6c671ca0c031a3591e69a290837ad
Orville James Balasa Santiago <santiagoorvillejames_bscs@plmun.edu.ph>
2025-03-28 00:12:43 +0800

    For 1.21.5

--- src/main/java/com/nad2040/elytrabombing/mixin/FallingBlockEntityMixin.java
+++ src/main/java/com/nad2040/elytrabombing/mixin/FallingBlockEntityMixin.java
@@ -9,9 +9,10 @@ import org.spongepowered.asm.mixin.Shadow;
 @Mixin(FallingBlockEntity.class)
 public abstract class FallingBlockEntityMixin implements ElytraBombingMod.FBEInterface {
     @Shadow
-    private BlockState block;
+    private BlockState field_7188; // Correct field name based on mappings
 
+    @Override
     public void setBlock(BlockState block) {
-        this.block = block;
+        this.field_7188 = block; // Assign to the shadowed field
     }
 }
