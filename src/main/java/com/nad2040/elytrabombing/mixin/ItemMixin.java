d804448b3f9b178b78f78a8e05e4e9bbd94221e4
Dan Liu <nad2040@gmail.com>
2025-05-25 13:16:20 -0400

    anvils hurt

--- src/main/java/com/nad2040/elytrabombing/mixin/ItemMixin.java
+++ src/main/java/com/nad2040/elytrabombing/mixin/ItemMixin.java
@@ -49,6 +49,7 @@ public class ItemMixin {
 				anvilEntity.timeFalling = 1; // allows anvils to work on 1.18* by avoiding `(this.timeFalling++ == 0)`
 				anvilEntity.setPosition(position);
 				anvilEntity.setVelocity(velocity.multiply(1.2));
+                anvilEntity.setHurtEntities(1.0f, 40);
 				if (usedItemStack.isOf(Items.ANVIL)) 		 ((ElytraBombingMod.FBEInterface) anvilEntity).setBlock(Blocks.ANVIL.getDefaultState());
 				if (usedItemStack.isOf(Items.CHIPPED_ANVIL)) ((ElytraBombingMod.FBEInterface) anvilEntity).setBlock(Blocks.CHIPPED_ANVIL.getDefaultState());
 				if (usedItemStack.isOf(Items.DAMAGED_ANVIL)) ((ElytraBombingMod.FBEInterface) anvilEntity).setBlock(Blocks.DAMAGED_ANVIL.getDefaultState());
