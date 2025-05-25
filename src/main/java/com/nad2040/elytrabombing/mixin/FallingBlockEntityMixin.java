a7d4fe00e52e634fe6da47b654aeefc97b257dab
Dan Liu <nad2040@gmail.com>
2025-05-25 10:03:11 -0400

    fix shadow

--- src/main/java/com/nad2040/elytrabombing/mixin/FallingBlockEntityMixin.java
+++ src/main/java/com/nad2040/elytrabombing/mixin/FallingBlockEntityMixin.java
@@ -9,10 +9,10 @@ import org.spongepowered.asm.mixin.Shadow;
 @Mixin(FallingBlockEntity.class)
 public abstract class FallingBlockEntityMixin implements ElytraBombingMod.FBEInterface {
     @Shadow
-    private BlockState field_7188; // Correct field name based on mappings
+    private BlockState blockState;
 
     @Override
     public void setBlock(BlockState block) {
-        this.field_7188 = block; // Assign to the shadowed field
+        this.blockState = block;
     }
 }
